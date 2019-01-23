202e8afaff5417909c6d9659e0b96a00bfe70fc0
TomasSB <tomiteror96@gmail.com>
2019-01-23 09:22:45 +0000

    first commit tomas

---
+++ util/validator.php
@@ -0,0 +1,85 @@
+<?php
+    function validateLetter ($letter){
+        if(!preg_match("/^[a-zA-Z\s]*$/", $letter)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    } 
+
+    function validateEmail ($email){
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false; 
+        }
+        else{
+            return true;
+        }
+    }
+
+    function checkPasswords ($password){
+        if (checkLength ($password) && 
+            checkNumbers ($password) &&
+            checkLetters ($password) &&
+            checkSpecialCharacters ($password)){
+                return true;
+            }
+            else {
+                return false;
+            }
+    }
+
+    function checkLength ($password){
+        if(preg_match("/.{9,}/",$password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function checkNumbers ($password){
+        if(preg_match("/[0-9]+/",$password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function checkLetters ($password){
+        if(preg_match("/[a-zA-Z]+/",$password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function checkSpecialCharacters ($password){
+        if(preg_match("/[\W]+/",$password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function checkDate ($date){
+        if(preg_match("/[0-9]{4}[-][0-9]{2}[-][0-9]{2}/",$date)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function checkUserName ($name){
+        if(!preg_match("/[a-zA-Z0-9\s]{3,15}/", $letter)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+?>

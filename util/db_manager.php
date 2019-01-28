c7f2b37650a23e1c635ae7ed7af712cb1b30d295
alvaromd28 <dominguezalvaro99@gmail.com>
2019-01-28 13:14:51 +0000

    Alvaro commit

--- util/db_manager.php
+++ util/db_manager.php
@@ -35,7 +35,7 @@
 
         if (mysqli_num_rows($result) > 0) {
             while($row = mysqli_fetch_assoc($result)) {
-                echo . $row["msg"]. "<br>";
+                echo  $row["msg"]. "<br>";
             }
         } else {
             echo "0 results";

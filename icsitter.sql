60111d8232dc9bbfce7399eae07765006f4bfb37
alvaromd28 <dominguezalvaro99@gmail.com>
2019-01-28 12:58:36 +0000

    Alvaro commit

--- icsitter.sql
+++ icsitter.sql
@@ -2,17 +2,17 @@ CREATE DATABASE icsitter;
 
 USE icsitter;
 
-CREATE TABLE user (
+CREATE TABLE `user` (
     id INT(11) AUTO_INCREMENT,
     userName VARCHAR(20) NOT NULL,
     `name` VARCHAR(20) NOT NULL,
     first_surname VARCHAR(50) NOT NULL,
     birthday date NOT NULL, 
     mail VARCHAR(50) NOT NULL,
-    reg_date DEFAULT TIMESTAMP,
-    password varchar(8) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `password` varchar(50) NOT NULL,
     PRIMARY KEY (id)
-);
+)
 
 CREATE TABLE msg (
     id INT(11) AUTO_INCREMENT,
